93fd350a43ad010b7b8dea255dfa858eab542bf1
wengel.eth <s.williamengel@gmail.com>
2022-04-28 15:19:06 -0600

    reformatting.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
 module.exports = function(eleventyConfig) {
+
+	let markdownIt = require("markdown-it");
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true
+	};
+
+	eleventyConfig.setLibrary("md", markdownIt(options));
+
 	eleventyConfig.setNunjucksEnvironmentOptions({
 		throwOnUndefined: true,
 		autoescape: false, // warning: don’t do this!
 	});
+
 	return {
 		htmlTemplateEngine: "njk",
 		dir: {
