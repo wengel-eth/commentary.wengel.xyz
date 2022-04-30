508f1e4a231059a8bbe5e6b9c1ec9437870f23b7
wengel.eth <54996189+wengel-eth@users.noreply.github.com>
2022-04-29 22:54:38 -0600

    styling.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,15 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.setNunjucksEnvironmentOptions({
 		throwOnUndefined: true,
-		autoescape: false, // warning: don’t do this!
+		autoescape: false // warning: don’t do this!
 	});
-
+	eleventyConfig.addPassthroughCopy({'src/styles' : 'styles'});
 	return {
 		htmlTemplateEngine: "njk",
+		passthroughFileCopy: true,
 		dir: {
 			input: 'src',
+			output: '_site',
 			layouts: '_layouts'
 		}
 	}
